8423dc5fa977dc259f32d06577cc0f9438458074
Franco Bellu <franco.bellu@gmail.com>
2025-01-22 23:21:30 +0100

    Initial Commit

---
+++ AdevintaUsers/AdevintaUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdevintaUsersApp.swift
+//  AdevintaUsers
+//
+//  Created by Franco Bellu on 22/1/25.
+//
+
+import SwiftUI
+
+@main
+struct AdevintaUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
